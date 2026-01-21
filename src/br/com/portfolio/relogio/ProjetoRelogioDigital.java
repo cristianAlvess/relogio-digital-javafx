d86ef718193e63eb0f6c41e13737f9235ffc9d08
Cristian Alves <cristian.alvess.dev@gmail.com>
2026-01-21 08:31:43 -0300

    Comentarios acrescentados

--- src/br/com/portfolio/relogio/ProjetoRelogioDigital.java
+++ src/br/com/portfolio/relogio/ProjetoRelogioDigital.java
@@ -1,17 +1,27 @@
 package br.com.portfolio.relogio;
 
+// JavaFX - Animação
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+
+// JavaFX - Estrutura da aplicação
 import javafx.application.Application;
+import javafx.stage.Stage;
+
+// JavaFX - Interface gráfica
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+
+// JavaFX - Utilitários
 import javafx.util.Duration;
+
+// Java - Data e Hora
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
 
 public class ProjetoRelogioDigital extends Application {
 
